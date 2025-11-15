79c520d3061fd8b85b738e2c7b00e5c86c81192c
Miura Toya <71395029+MiuraToya@users.noreply.github.com>
2025-11-15 16:31:23 +0900

    add: CDKの環境構築 (#32)
    - CDKの初期化
    - eslint prettierセットアップ

---
+++ infra/eslint.config.mjs
@@ -0,0 +1,14 @@
+import { defineConfig } from 'eslint/config';
+import eslint from '@eslint/js';
+import tseslint from 'typescript-eslint';
+import eslintConfigPrettier from 'eslint-config-prettier';
+
+export default defineConfig(
+  eslint.configs.recommended,
+  tseslint.configs.strict,
+  tseslint.configs.stylistic,
+  eslintConfigPrettier,
+  {
+    ignores: ['cdk.out/', '*.js', '*.d.ts', 'node_modules/'],
+  }
+);
